ad38052bb6ade0cecb4f88717d0228de8adf20e0
JNturtle <31470045+JNturtle@users.noreply.github.com>
2020-03-26 13:35:56 +0800

    Add files via upload

---
+++ F.py
@@ -0,0 +1,73 @@
+import win32api
+import win32con
+import os
+#
+defaultVersion = "2.13.20.0"
+infoDict = {}
+ID = infoDict
+#
+def getOSBit():
+    import platform
+    if platform.architecture()[0] == "64bit": return "x64"
+    else: "x84"
+def checkVersion(COM_path):
+    if not os.path.isfile(COM_path): 
+        COM_path = "不存在"
+        LastestVersion, LastestVersionName = "不存在", "不存在"
+    else:
+        LastestVersionName = win32api.GetFileVersionInfo(COM_path, '\\StringFileInfo\\040904b0\\ProductName')
+        LastestVersion = win32api.GetFileVersionInfo(COM_path, '\\StringFileInfo\\040904b0\\FileVersion')
+    return  COM_path, LastestVersionName, LastestVersion
+def getRegCOMPath():
+    try:
+        return win32api.RegQueryValue(win32con.HKEY_LOCAL_MACHINE, r"SOFTWARE\Classes\WOW6432Node\TypeLib\{75AAD71C-8F4F-4F1F-9AEE-3D41A8C9BA5E}\1.0\0\win32")
+    except:
+        pass
+    return "不存在"   
+def getCCModuleCOMPath():
+    import comtypes.client as cc    
+    ModulePyName = "_75AAD71C_8F4F_4F1F_9AEE_3D41A8C9BA5E_0_1_0.py"
+    ModulePyPath = "\\".join([cc.gen_dir,ModulePyName])
+    if not os.path.isfile(ModulePyPath): return "不存在"
+    with open(ModulePyPath, 'r') as f:
+        f.readline(1000)
+        data = f.readline(1000)
+        f.close()
+    return data[16:-2].replace("\\\\","\\")
+def delCCModule():   
+    def delFile(path, name):
+        for fileName in os.listdir(path)[:]:
+            if fileName.find(name) != -1:             
+                filePath = "\\".join([path, fileName])
+                print("刪除", filePath)
+                os.remove(filePath)
+            elif len(fileName.split(".")) == 1:
+                filePath = "\\".join([path, fileName])
+                delFile(filePath, name)
+    import comtypes.client as cc   
+    delFile(cc.gen_dir, "_75AAD71C_8F4F_4F1F_9AEE_3D41A8C9BA5E_0_1_0")
+def reset():
+    infoDict['OSBit'] = getOSBit()
+    infoDict['LastestCOM'] = {}
+    COM_dir = "\\".join([os.path.split(__file__)[0], "元件", infoDict['OSBit']])
+    if not os.path.isdir(COM_dir): infoDict['LastestCOM']['dir'] ="不存在"
+    else: infoDict['LastestCOM']['dir'] = COM_dir
+
+    COM_Names = ['LastestCOM', 'regCOM', 'CCCOM']
+    COM_Paths = []
+    COM_Paths.append("\\".join([COM_dir, "SKCOM.dll"]))
+    COM_Paths.append(getRegCOMPath())
+    COM_Paths.append(getCCModuleCOMPath())
+    
+    for COM_Name, COM_Path in zip(COM_Names, COM_Paths):
+        path, name, version = checkVersion(COM_Path)    
+        if COM_Name not in infoDict: infoDict[COM_Name] = {}
+        infoDict[COM_Name]['path'] = path
+        infoDict[COM_Name]['name'] = name
+        infoDict[COM_Name]['version'] = version
+
+reset()
+#
+
+
+
---
+++ 群益SKCOM工具.py
@@ -0,0 +1,106 @@
+import sys
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QApplication,QDialog, QMessageBox
+from F import *
+
+class SKCOMUpdater(QDialog):
+    def __init__(self):
+        super(SKCOMUpdater,self).__init__()
+        loadUi(r'interface.ui',self)
+        self.resetLabel()
+        self.Refresh.clicked.connect(self.resetLabel)
+        self.Check.clicked.connect(self.Check_clicked)
+
+    def resetLabel(self):
+        reset()
+        self.OSBit.setText(ID['OSBit'])
+        self.lastestVersion.setText(ID['LastestCOM']['version'])
+        self.lastestVersionName.setText(ID['LastestCOM']['name'])
+        self.lastestVersionPath.setText(ID['LastestCOM']['path'])        
+        self.regVersion.setText(ID['regCOM']['version'])
+        self.regVersionName.setText(ID['regCOM']['name'])
+        self.regVersionPath.setText(ID['regCOM']['path'])       
+        self.moduleVersion.setText(ID['CCCOM']['version'])
+        self.moduleVersionName.setText(ID['CCCOM']['name'])
+        self.moduleVersionPath.setText(ID['CCCOM']['path'])     
+        
+        pass
+
+    def Check_clicked(self):
+        """
+        檢查三者版本的差異，並顯示提示視窗
+        """
+        LCV = ID['LastestCOM']['version']
+        RCV = ID['regCOM']['version']
+        MCV = ID['CCCOM']['version']
+        if LCV != defaultVersion:
+            title = "資料夾內的COM元件不是最新版本"
+            content = "\n".join(["資料夾內的COM元件 {:}".format(LCV), "不是最新版本 {:}".format(SysVersion), "請下載最新群益API的COM替換"])
+            showMegBox(self, "critical", title, content)
+        elif LCV == "不存在":
+            title = "找不到資料夾內的COM元件"
+            content = "\n".join(["找不到資料夾內的COM元件", "請重新下載本程式"])
+            showMegBox(self, "critical", title, content)
+        elif RCV == "不存在":
+            title = "尚未註冊或找不到COM元件"
+            content = "\n".join(["尚未註冊或找不到COM元件", "請使用「系統管理員身分執行」元件中的「install.bat」以註冊COM元件"])
+            showMegBox(self, "critical", title, content)
+        elif LCV != RCV:
+            title = "COM註冊版本不是最新版本"
+            content = "\n".join(["COM註冊版本不是最新版本", "請使用「系統管理員身分執行」元件中的「Uninstall.bat」「install.bat」以重新註冊COM元件"])
+            showMegBox(self, "critical", title, content)
+        elif ID['LastestCOM']['name'] != ID['regCOM']['name']:
+            title = "COM註冊版本位元不是正確位元"
+            content = "\n".join(["COM註冊版本位元不是正確位元", "請使用「系統管理員身分執行」元件中的「Uninstall.bat」「install.bat」以重新註冊COM元件"])
+            showMegBox(self, "critical", title, content)
+        elif ID['CCCOM']['name'] != ID['regCOM']['name']:
+            title = "COM註冊版本位元與ComtypesClient模型使用的COM不同"
+            content = "\n".join(["COM註冊版本位元與ComtypesClient模型使用的COM不同", "請重新註冊COM元件或再次檢查所使用的COM版本"])
+            showMegBox(self, "critical", title, content)
+            title = "重新生成ComtypesClient模型"
+            content = "\n".join(["重新生成ComtypesClient模型？", "若版本有差異，可能會導致群益API無法正常使用！"])
+            if showMegBox(self, "info", title, content, YesNo = True): 
+                delCCModule()    
+                import comtypes.client as cc
+                cc.GetModule(ID['LastestCOM']['path'])
+        elif RCV != MCV and MCV != "不存在":
+            title = "COM註冊版本與ComtypesClient模型版本不同"
+            content = "\n".join(["COM註冊版本與ComtypesClient模型版本不同", "請檢查是否程式使用的COM是否為最新版本"])
+            showMegBox(self, "critical", title, content)
+            title = "刪除舊版本的ComtypesClient模型"
+            content = "\n".join(["是否需要刪除舊版本的ComtypesClient模型？", "若版本有差異，可能會導致群益API無法正常使用！"])
+            if showMegBox(self, "info", title, content, YesNo = True): delCCModule()            
+        # info 等級
+        elif MCV == "不存在":
+            title = "尚未生成新版COM的py模組"
+            content = "\n".join(["尚未生成新版COM模組", "可以正常執行群益API"])
+            showMegBox(self, "info", title, content)
+            title = "從更新器中產生新版COM的py模組"
+            content = "\n".join(["是否從更新器中產生新版COM的py模組？"])
+            if showMegBox(self, "info", title, content, YesNo = True): 
+                import comtypes.client as cc
+                cc.GetModule(ID['LastestCOM']['path'])
+        else:
+            title = "沒有任何異常"
+            content = "\n".join(["沒有檢查到任何錯誤"]) 
+            showMegBox(self, "info", title, content)
+
+        self.resetLabel()
+
+
+def showMegBox(parent, level, title, content, YesNo = False):
+    if not YesNo:
+        if level == "critical":
+            QMessageBox.critical(parent, title, content, QMessageBox.Yes)
+        else:
+            QMessageBox.information(parent, title, content, QMessageBox.Yes)
+    else:
+        reply = QMessageBox.information(parent, title, content, QMessageBox.Yes|QMessageBox.No, QMessageBox.No)
+        if reply == 16384: return True
+        else: return False
+
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    Window = SKCOMUpdater()
+    Window.show()
+    sys.exit(App.exec_())
